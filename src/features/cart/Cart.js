0e0a52c65323b289727308817d645d2f58747169
Sean <sean@merrehill.co.uk>
2021-09-27 16:44:32 +0100

    making changes to cart

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -11,7 +11,8 @@ import { getUserCart,
     erroredCart, 
     redirectRequired 
 } from "./cartSlice"
-import Tile from "../../components/tile/Tile";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons"
 import Checkout from "../../components/checkout/Checkout";
 import {Elements} from '@stripe/react-stripe-js';
 import { loadStripe } from "@stripe/stripe-js";
@@ -32,6 +33,8 @@ export default function Cart(){
     const cartErroed = useSelector(erroredCart);
     const items = useSelector(cartItems);
     const redirect = useSelector(redirectRequired);
+    const [updateCart, setUpdateCart] = useState([]);    
+    const [updateCartButtonDisabled, setUpdateCartButtonDisabled] = useState(true);
 
     useEffect(() => {
         dispatch(getUserCart());
@@ -55,6 +58,29 @@ export default function Cart(){
         setShowPayment(setPayment = !setPayment)
     } 
 
+    const handleProductIncrease = (id) => {
+        console.log(updateCart.length)
+        let cart = updateCart;
+        cart.push({"id": id, "diff": 1})
+        setUpdateCartButtonDisabled(false)
+        setUpdateCart(cart);
+    }
+
+    const handleProductDecrease = (id) => {
+        let cart = updateCart;
+        cart.push({"id": id, "diff": -1})
+        setUpdateCartButtonDisabled(false)
+        setUpdateCart(cart);
+    }        
+
+    const handleUpdateSubmit = (e) => {
+        let update = updateCart.reduce((accu, prev) => {
+            
+            return prev['diff']
+        })
+        console.log(update)
+    }
+
     const getCartPrice = (items) => {
         let total = 0;
         items.forEach(e => {
@@ -70,10 +96,39 @@ export default function Cart(){
                 ?
                <div className="cart">
                     {items.map((e, i) => (
-                        <Tile key={i} productDetails={e} underlineRequired={i + 1 === items.length ? false : true} />                    
+                        <div key={i}>
+                            {Object.keys(e).length 
+                            ? 
+                            <div className="product">
+                                <div className="details">
+                                    <div className="info">
+                                        <h2>{e.name} ({e.quantity})</h2>
+                                        <div className="itemOptions">
+                                            <div className="add" onClick={() => {handleProductIncrease(e.id)}}>
+                                                <span><FontAwesomeIcon icon={faPlus} /></span>
+                                            </div>
+                                            <div className="minus" onClick={() => {handleProductDecrease(e.id)}}>
+                                                <span><FontAwesomeIcon icon={faMinus} /></span>
+                                            </div>
+                                        </div>                        
+                                    </div>
+                                    <div className="accumulativePrice">
+                                            <h2>£{e.price * e.quantity}</h2>
+                                    </div>
+                                </div>
+                                <div>
+                                    <hr />
+                                </div>
+                            </div>      
+                            :
+                            ''      
+                            }
+
+                        </div>               
                     ))}  
-                    <div className="checkout">
-                        <button onClick={handlePaymentContainer}>Checkout</button>
+                    <div className="checkoutContainer">
+                        <button className="checkout" onClick={handlePaymentContainer}>Checkout</button>
+                        <button className={!updateCartButtonDisabled ? 'updateCart' : 'updateCartDisabled'} disabled={updateCartButtonDisabled} onClick={handleUpdateSubmit}>Update cart</button>
                         <h2>Total price: £{getCartPrice(items)}</h2>
                     </div> 
                     <div className={showPayment ? 'paymentContainer' : 'paymentContainerHide'}>
@@ -85,7 +140,7 @@ export default function Cart(){
                 :
                 <div className="cart">
                     <div className="checkoutDisabled">
-                        <button disabled={true} onClick={handlePaymentContainer}>Checkout</button>
+                        <button disabled={true}>Checkout</button>
                         <h2>Total price: £0</h2>
                     </div>
                 </div>    
